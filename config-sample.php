e5a5cf559d62aebfa51740df9090f7163a0fdbe7
Alfred Wester <alfredwester@gmail.com>
2011-12-21 14:08:29 +0100

    [Added] config sample file

---
+++ config-sample.php
@@ -0,0 +1,14 @@
+<?php
+//Database configuration
+
+define('DB_HOST', 'localhost');
+
+define('DB_USER', 'username');
+
+define('DB_PASSWORD', 'password');
+
+define('DB_PREFIX', 'zazu_');
+
+define('DB_DATABASE', 'zazu');
+
+?>
