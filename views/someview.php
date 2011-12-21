0be877916333f49856e9afa2c6dba60f41ed5361
Alfred Wester <alfredwester@gmail.com>
2011-12-21 13:00:39 +0100

    [Improvement] basic hmtl format in view

--- views/someview.php
+++ views/someview.php
@@ -1,11 +1,6 @@
-<html>
-	<head>
-		<title>A full working Tiny MVC</title>
-	</head>
 	<body>
-	
-		<h1>A small php MVC framework</h1>
-		<?php echo $first.' '.$second.' '.$last;?>
-
+		<?=$content?>
+		
+		<footer><?=$footer?></footer>
 	</body>
 </html>
