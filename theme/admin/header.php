3127b9aa8ace52c83541200ca361bbbf281363a4
Alfred Wester <alfredwester@gmail.com>
2012-01-11 17:27:24 +0100

    removed blueprint from theme css and using /include/blueprint instead

--- theme/admin/header.php
+++ theme/admin/header.php
@@ -1,42 +1,41 @@
 <html>
-    <head>
-        <title>Administration --Zazu cms--</title>
+	<head>
+		<title>Administration --Zazu cms--</title>
 		<meta name="description" content="This is the administration interface for Zazu cms" />
 		<meta name="keyword" content="Admin, cms, mvc, zazu, administation" />
-		<link rel="stylesheet" href="/theme/default/css/blueprint/screen.css" type="text/css" media="screen, projection">
-		<link rel="stylesheet" href="/theme/default/css/blueprint/print.css" type="text/css" media="print">
-		<link rel="stylesheet" href="/theme/default/css/buttons.css" type="text/css" media="screen, projection">
+		<link rel="stylesheet" href="/include/blueprint/screen.css" type="text/css" media="screen, projection">
+		<link rel="stylesheet" href="/include/blueprint/print.css" type="text/css" media="print">
 		<!--[if lt IE 8]>
-		<link rel="stylesheet" href="/theme/default/css/blueprint/ie.css" type="text/css" media="screen, projection">
+		<link rel="stylesheet" href="/include/blueprint/ie.css" type="text/css" media="screen, projection">
 		<![endif]-->
 		<link rel="stylesheet" href="/theme/default/css/zazu.css" type="text/css" media="all">
+		<link rel="stylesheet" href="/theme/default/css/buttons.css" type="text/css" media="screen, projection">
 		<?=$head;?>
 	</head>
-    <body>
-        <div class="container">
-            <header class="header span-24">
-                <h4>Zazu Administration</h4>
-            </header>
-            <nav class="span-24 menu buttons">
-                <?php
+	<body>
+		<div class="container">
+			<header class="header span-24">
+				<h4>Zazu Administration</h4>
+			</header>
+			<nav class="span-24 menu buttons">
+				<?php
 				foreach($admin_menu as $link) {
 				?>
 					<a href="<?=$link['menu_url'];?>" title="<?=$link['menu_title'];?>"><?=$link['menu_text'];?></a>
-				<?php	
+				<?php
 				}
 				?>
-            </nav>
+			</nav>
 			<hr>
 			<?php
-                if(isset($success)) {
+				if(isset($success)) {
 					echo "<div class=\"span-8 push-8 success\">".$success."</div>";
-                }
-                if(isset($errors)) {
-                    echo "<div class=\"span-8 push-8 error\">";
-                    foreach($errors as $val) {
-                        echo $val."<br>";
-                    }
-                    echo "</div>";
-                }
+				}
+				if(isset($errors)) {
+					echo "<div class=\"span-8 push-8 error\">";
+					foreach($errors as $val) {
+						echo $val."<br>";
+					}
+					echo "</div>";
+				}
 			?>
-
--- theme/default/header.php
+++ theme/default/header.php
@@ -1,30 +1,29 @@
 <html>
-    <head>
-        <title><?=$title?></title>
+	<head>
+		<title><?=$title?></title>
 		<meta name="description" content="<?=$meta_content?>">
 		<meta name="keyword" content="<?=$meta_keyword?>">
-        <link rel="stylesheet" href="/theme/default/css/blueprint/screen.css" type="text/css" media="screen, projection">
-        <link rel="stylesheet" href="/theme/default/css/blueprint/print.css" type="text/css" media="print">
-        <link rel="stylesheet" href="/theme/default/css/buttons.css" type="text/css" media="screen, projection">
-        <link rel="stylesheet" href="/theme/default/css/zazu.css" type="text/css" media="all">
-        <!--[if lt IE 8]>
-        <link rel="stylesheet" href="/theme/default/css/blueprint/ie.css" type="text/css" media="screen, projection">
-        <![endif]-->
-    </head>
-    <body>
-        <div class="container showgrid">
-            <header class="header span-24">
-                <h4><?=$site_title?></h4>
-                <p><?=$site_subtitle?></p>
-            </header>
-            <nav class="span-24 menu buttons">
-                <?php
+		<link rel="stylesheet" href="/include/blueprint/screen.css" type="text/css" media="screen, projection">
+		<link rel="stylesheet" href="/include/blueprint/print.css" type="text/css" media="print">
+		<!--[if lt IE 8]>
+		<link rel="stylesheet" href="/include/blueprint/ie.css" type="text/css" media="screen, projection">
+		<![endif]-->
+		<link rel="stylesheet" href="/theme/default/css/buttons.css" type="text/css" media="screen, projection">
+		<link rel="stylesheet" href="/theme/default/css/zazu.css" type="text/css" media="all">
+	</head>
+	<body>
+		<div class="container showgrid">
+			<header class="header span-24">
+				<h4><?=$site_title?></h4>
+				<p><?=$site_subtitle?></p>
+			</header>
+			<nav class="span-24 menu buttons">
+				<?php
 				foreach($menu_1 as $link) {
 				?>
 					<a href="<?=$link['link_url'];?>" title="<?=$link['link_title'];?>"><?=$link['link_text'];?></a>
-				<?php	
+				<?php
 				}
 				?>
-            </nav>
-            <hr>
-
+			</nav>
+			<hr>
