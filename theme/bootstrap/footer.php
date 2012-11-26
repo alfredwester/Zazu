0269476cfe2190897c717092f654f076a54bb104
Alfred Wester <alfredwester@gmail.com>
2012-11-26 17:09:07 +0100

    Added stickyFooter that is used in bootstrap admin theme

--- theme/bootstrap/footer.php
+++ theme/bootstrap/footer.php
@@ -1,8 +1,15 @@
-			<hr>
-			<footer>
-				<p>Zazu Administration</p>
-			</footer>
+			</div>
 		</div>
+		<footer id="footer">
+			<div class="divider"></div>
+			<div class="container">
+				<div class="row">
+					<div class="span12">
+						Zazu Administration
+					</div>
+				</div>
+			</div>
+		</footer>
 		<script type="text/javascript" src="<?php echo BASE_PATH;?>/include/jquery/jquery.min.js" ></script>
 		<script type="text/javascript" src="<?php echo BASE_PATH;?>/include/bootstrap/js/bootstrap.min.js" ></script>
     </body>
--- theme/bootstrap/header.php
+++ theme/bootstrap/header.php
@@ -6,47 +6,54 @@
 		<meta name="description" content="This is the administration interface for Zazu cms" />
 		<meta name="keyword" content="Admin, cms, mvc, zazu, administation" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="<?php echo BASE_PATH;?>/include/stickyfooter/stickyfooter.css" rel="stylesheet" media="screen" />
 		<link href="<?php echo BASE_PATH;?>/include/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen" />
 		<link href="<?php echo BASE_PATH;?>/theme/bootstrap/css/zazu.css" rel="stylesheet" media="screen" />
+		<!--[if !IE 7]>
+			<style type="text/css">
+				#wrap {display:table;height:100%}
+			</style>
+		<![endif]-->
 		<?=$head;?>
 	</head>
 	<body>
-		<div class="navbar navbar-inverse navbar-fixed-top">
-			<div class="navbar-inner">
-				<nav class="container">
-					<a class="btn btn-navbar" data-target=".nav-collapse" data-toggle="collapse"></a>
-					<a class="brand" href="#">Zazu Administration</a>
-					<div class="nav-collapse collapse">
-						<ul class="nav">
-						<?php
-						foreach($admin_menu as $link) {
-						?>
-							<li><a href="<?php echo BASE_PATH.$link['menu_url'];?>" title="<?=$link['menu_title'];?>"><?=$link['menu_text'];?></a></li>
-						<?php
-						}
-						?>
-						</ul>
-					</div>
-				</nav>
+		<div id="wrap">
+			<div class="navbar navbar-inverse navbar-fixed-top">
+				<div class="navbar-inner">
+					<nav class="container">
+						<a class="btn btn-navbar" data-target=".nav-collapse" data-toggle="collapse"></a>
+						<a class="brand" href="#">Zazu Administration</a>
+						<div class="nav-collapse collapse">
+							<ul class="nav">
+							<?php
+							foreach($admin_menu as $link) {
+							?>
+								<li><a href="<?php echo BASE_PATH.$link['menu_url'];?>" title="<?=$link['menu_title'];?>"><?=$link['menu_text'];?></a></li>
+							<?php
+							}
+							?>
+							</ul>
+						</div>
+					</nav>
+				</div>
 			</div>
-		</div>
-		<div class="main container">
-			<?php
-				if(isset($success)) {
-					echo "<div class=\"row\"><div class=\"span4 offset4 alert alert-success\">".$success." <a href=\"#\" class=\"close\" data-dismiss=\"alert\">×</a></div></div>";
-				}
-				if(isset($notice)) {
-					echo "<div class=\"row\"><div class=\"span4 offset4 alert\">".$notice."<a href=\"#\" class=\"close\" data-dismiss=\"alert\">×</a></div></div>";
-				}
-				if(isset($info)) {
-					echo "<div class=\"row\"><div class=\"span4 offset4 alert alert-info\">".$info."<a href=\"#\" class=\"close\" data-dismiss=\"alert\">×</a></div></div>";
-				}
-				if(isset($errors)) {
-					echo "<div class=\"row\"><div class=\"span4 offset4 alert alert-error\">";
-					echo "<a href=\"#\" class=\"close\" data-dismiss=\"alert\">×</a>";
-					foreach($errors as $val) {
-						echo $val."<br>";
+			<div id="main" class="container">
+				<?php
+					if(isset($success)) {
+						echo "<div class=\"row\"><div class=\"span4 offset4 alert alert-success\">".$success." <a href=\"#\" class=\"close\" data-dismiss=\"alert\">×</a></div></div>";
 					}
-					echo "</div></div>";
-				}
-			?>
+					if(isset($notice)) {
+						echo "<div class=\"row\"><div class=\"span4 offset4 alert\">".$notice."<a href=\"#\" class=\"close\" data-dismiss=\"alert\">×</a></div></div>";
+					}
+					if(isset($info)) {
+						echo "<div class=\"row\"><div class=\"span4 offset4 alert alert-info\">".$info."<a href=\"#\" class=\"close\" data-dismiss=\"alert\">×</a></div></div>";
+					}
+					if(isset($errors)) {
+						echo "<div class=\"row\"><div class=\"span4 offset4 alert alert-error\">";
+						echo "<a href=\"#\" class=\"close\" data-dismiss=\"alert\">×</a>";
+						foreach($errors as $val) {
+							echo $val."<br>";
+						}
+						echo "</div></div>";
+					}
+				?>
