0f5cec0a6a5e98e8c86381df1ca419e22f3e2d04
Alfred Wester <alfredwester@gmail.com>
2015-02-21 19:09:49 +0100

    Calendar plugin: Added text to display if no events was found

--- plugins/calendar/calendar.php
+++ plugins/calendar/calendar.php
@@ -23,28 +23,32 @@ class Calendar implements IPluginController {
 	protected function draw_calendar() {
 
 		$calendar = "<div class=\"calendar\">";
-		$events = $this->calendar_model->get_events()['events'];
-		foreach($events as $event) {
-			extract($event);
-			$time = strtotime($event_date);
-			$event_month = date('M Y', $time);
-			$event_time = date('G:i', $time);
-			$event_date = date('j', $time);
-			$calendar.= "<div class=\"calendar-event-row\">
+		$events = $this->calendar_model->get_events();
+		if (empty($events)) {
+			$calendar .= "<p class=\"calendar-no-events\">Inga aktuella händelser hittades, återkom inom kort.</p>";
+		} else {
+			foreach ($events['events'] as $event) {
+				extract($event);
+				$time = strtotime($event_date);
+				$event_month = date('M Y', $time);
+				$event_time = date('G:i', $time);
+				$event_date = date('j', $time);
+				$calendar .= "<div class=\"calendar-event-row\">
 				<div class=\"calendar-event-container\">
 					<div class=\"calendar-event-date\">
-						<span class=\"date\">".$event_date."</span>
-						<span class=\"month\">".$event_month."</span>
-						<span class=\"time\">".$event_time."</span>
+						<span class=\"date\">" . $event_date . "</span>
+						<span class=\"month\">" . $event_month . "</span>
+						<span class=\"time\">" . $event_time . "</span>
 					</div>
 					<div class=\"calendar-event-info clearfix\">
-						<h5>".$event_name."</h5>
-						<p>".$event_description."</p>
+						<h5>" . $event_name . "</h5>
+						<p>" . $event_description . "</p>
 					</div>
 				</div>
 			</div>";
+			}
 		}
-		$calendar.= "</div>";
+		$calendar .= "</div>";
 
 		return $calendar;
 	}
